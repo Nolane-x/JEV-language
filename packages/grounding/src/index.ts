d97b7a71243cafc825ded77f74608b12463eea1c
Nolane-x <casioreview20@gmail.com>
2026-09-19 18:12:41 +0700

    feat: export M18 open-world language grounding APIs

--- packages/grounding/src/index.ts
+++ packages/grounding/src/index.ts
@@ -3,3 +3,4 @@ export * from "./pipeline.ts";
 export * from "./controlled-corpus.ts";
 export * from "./expanded-english.ts";
 export * from "./controlled-corpus-vi.ts";
+export * from "./open-world-language.ts";
