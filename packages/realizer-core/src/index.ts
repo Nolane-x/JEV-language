b228d354d9e843a913de0d65ca8df59e04426934
Nolane-x <casioreview20@gmail.com>
2026-09-19 11:47:00 +0700

    feat: carry M6 English expansion for packages/realizer-core/src/index.ts

--- packages/realizer-core/src/index.ts
+++ packages/realizer-core/src/index.ts
@@ -2,3 +2,5 @@ export * from "./controlled-en.ts";
 export * from "./controlled-vi.ts";
 export * from "./controlled-corpus-en.ts";
 export * from "./planning.ts";
+export * from "./naturalness.ts";
+export * from "./expanded-en.ts";
