93d8e09b3b3c87a3f10a84c625248fcd9d74817e
Nolane-x <casioreview20@gmail.com>
2026-09-20 15:20:35 +0700

    feat: export M19 human-study kit

--- packages/evaluation-core/src/index.ts
+++ packages/evaluation-core/src/index.ts
@@ -1082,6 +1082,6 @@ export * from "./dashboard.ts";
 
 export * from "./release-gates.ts";
 
-export * from "./natural-conversation.ts";
+export * from "./natural-conversation.ts";\nexport * from "./m19-study-kit.ts";
 
 export * from "./v1-conformance.ts";
