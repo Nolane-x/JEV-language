bd8111e3dd68b1fb18b6f916d6536f18e17b2f15
Nolane-x <casioreview20@gmail.com>
2026-09-20 17:55:45 +0700

    feat: export conversation style memory

--- packages/realizer-core/src/index.ts
+++ packages/realizer-core/src/index.ts
@@ -5,6 +5,7 @@ export * from "./planning.ts";
 export * from "./naturalness.ts";
 export * from "./conversation-candidates.ts";
 export * from "./conversation-lattice.ts";
+export * from "./conversation-style-memory.ts";
 export * from "./expanded-en.ts";
 export * from "./expanded-document-en.ts";
 export * from "./controlled-corpus-vi.ts";
